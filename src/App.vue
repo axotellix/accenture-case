9de69f974e52a04dffb1c113b232e38c4bb1d1d4
axotellix <RSAndrey@list.ru>
2021-09-04 15:25:31 +0300

    Created > basic template

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,19 @@
+
+<!-- [ template ] -->
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Sidebar msg="Welcome to Your Vue.js App" />
 </template>
 
+
+<!-- [ scripts ] -->
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Sidebar from './components/Sidebar.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    name: 'App',
+    components: {
+        Sidebar
+    }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/Sidebar.vue
@@ -0,0 +1,17 @@
+
+<!-- [ template ] -->
+<template>
+    <div>{{ msg }}</div>
+</template>
+
+
+<!-- [ scripts ] -->
+<script>
+export default {
+    name: 'HelloWorld',
+    props: {
+        msg: String
+    }
+}
+</script>
+
